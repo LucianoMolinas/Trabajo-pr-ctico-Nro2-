4e7d5af39d700de0b44bdaa9f64fdca9e3e05807
lucianomolinas <lucianomolinas74@gmail.com>
2025-09-27 20:57:18 -0300

    add: Rout Not Found

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -1,13 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, href } from "react-router-dom";
 import { Login } from "../views/Login";
 import { Messages } from "../views/Messages";
-
+import { NotFound } from "../views/NotFound"
 const RouterApp = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/chat" element={<Messages />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   )
