8a28dd0926491d176c7799dda10a1d8051c2ca6c
lucianomolinas <lucianomolinas74@gmail.com>
2025-09-27 13:40:33 -0300

    estilos del Sidebar

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -3,7 +3,19 @@ export default function Sidebar() {
     <div className="sidebar">
       <input type="text" placeholder="Search..." className="search" />
       <ul className="user-list">
-        {[].map((u, i) => (
+        <li className="user">
+          <img className="avatar" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4YreOWfDX3kK-QLAbAL4ufCPc84ol2MA8Xg&s" alt="" />
+          <div></div>
+          <div>
+            <strong>Vincent Porter</strong>
+          </div>
+        </li>
+
+
+
+
+
+        {/*{[].map((u, i) => (
           <li key={i} className="user">
             <img src={u.avatar} alt={u.name} className="avatar" />
             <div className={`status ${u.status}`}></div>
@@ -12,7 +24,7 @@ export default function Sidebar() {
               {u.lastSeen && <span className="last-seen"> - {u.lastSeen}</span>}
             </div>
           </li>
-        ))}
+        ))}*/}
       </ul>
     </div>
   )
