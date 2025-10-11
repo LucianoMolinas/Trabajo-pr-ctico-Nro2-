4723ac2791d1e0f3659746dd0664b2ca793f6de5
lucianomolinas <lucianomolinas74@gmail.com>
2025-10-11 13:17:48 -0300

    incorporacion de help

--- src/router/RouterApp.jsx
+++ src/router/RouterApp.jsx
@@ -3,7 +3,7 @@ import { Login } from "../views/Login";
 import { Messages } from "../views/Messages";
 import { NotFound } from "../views/NotFound"
 import ProtectedRoute from "../components/ProtectedRoute";
-
+import Help from "../views/Help";
 
 
 
@@ -18,6 +18,7 @@ const RouterApp = () => {
               <Messages />
             </ProtectedRoute>}
         />
+        <Route path="/help" element={<Help />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import logo from "../assets/images/logo.png"
 
 const Login = () => {
@@ -67,6 +67,7 @@ const Login = () => {
         {error && <p style={{ color: "red" }}>{error}</p>}
       </form>
       <p className="text-info"> Acceso restringido • Contenido privado</p>
+      <p className="text-info">Ir a <Link to="/help">Help</Link></p>
     </main>
   )
 }
